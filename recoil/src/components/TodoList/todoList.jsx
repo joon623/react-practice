231fd4e1b682b05cb84915b8195b17acf5680c99
jun <nnamjun94@gmail.com>
2022-04-28 00:01:00 +0900

    3th init

--- recoil/src/components/TodoList/todoList.jsx
+++ recoil/src/components/TodoList/todoList.jsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from 'recoil';
-import { filteredTodoListState, todoListState } from '../../store/todoList';
+import { filteredTodoListState } from '../../store/todoList';
 import TodoItem from './todoItem';
 import TodoItemCreator from './todoListCreater';
 import TodoListFilters from './todoListFilters';
@@ -8,7 +8,6 @@ import TodoListStats from './todoListStats';
 const TodoList = () => {
   const todoList = useRecoilValue(filteredTodoListState);
 
-  console.log(todoList);
   return (
     <>
       <TodoListStats />

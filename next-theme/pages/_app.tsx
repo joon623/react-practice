420c249b265f53b394811fe8a94a006e17402536
jun <nnamjun94@gmail.com>
2022-05-12 18:36:13 +0900

    add: media prefer-color-scheme

--- next-theme/pages/_app.tsx
+++ next-theme/pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-import "./index.css";
 import {ThemeProvider} from 'next-themes'
+
+const lightTheme = `
+  --text: black;
+  --background: white;
+  --hello: black;
+`;
+const darkTheme = `
+  --color-text: white;
+  --color-background: black;
+  --hello: purple;
+`;
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
@@ -9,10 +19,29 @@ function MyApp({Component, pageProps}: AppProps) {
             disableTransitionOnChange={true}
             defaultTheme="system"
             attribute="class"
-            // enableSystem={true}
-            // defaultTheme="system"
-            // attribute='data-theme'
         >
+            <style global jsx>{`
+              body {
+                ${lightTheme};
+              }
+
+              @media (prefers-color-scheme: dark) {
+                body {
+                  ${darkTheme}
+                }
+              }
+
+              body[data-theme='light'] {
+                ${lightTheme};
+              }
+
+              body[data-theme='dark'] {
+                ${darkTheme};
+              }
+
+            `}
+
+            </style>
             <Component {...pageProps} />
         </ThemeProvider>
     )
--- next-theme/pages/index.tsx
+++ next-theme/pages/index.tsx
@@ -16,7 +16,9 @@ const Home: NextPage = () => {
     }
 
     return (
-        <div>
+        <div
+            style={{background: "var(--hello)"}}
+        >
             The current theme is: {theme}
             <button onClick={() => setTheme('light')}>Light Mode</button>
             <button onClick={() => setTheme('dark')}>Dark Mode</button>
